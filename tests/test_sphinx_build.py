be85ec85e8c80a709e214d046c9257a34c232a19
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2021-05-10 22:11:12 +0100

    moved rst files

--- tests/test_sphinx_build.py
+++ tests/test_sphinx_build.py
@@ -8,4 +8,4 @@ class TestSphinxBuild(unittest.TestCase):
     def test_building_example_docs(self):
         """Builds documentation that contains a few example use cases"""
 
-        subprocess.check_output(['sphinx-build', '-b', 'html', './source', './build/html/'])
+        subprocess.check_output(['sphinx-build', '-b', 'html', './tests/example', './build/html/'])
