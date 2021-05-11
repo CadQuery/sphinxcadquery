0f8011453471632ed711b10573d80751724ed751
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2021-05-11 22:42:09 +0100

    removed class;

--- tests/test_sphinx_build.py
+++ tests/test_sphinx_build.py
@@ -2,9 +2,7 @@
 import subprocess
 
 
-class TestSphinxBuild():
+def test_building_example_docs():
+    """Builds documentation that contains a few example use cases"""
 
-    def test_building_example_docs(self):
-        """Builds documentation that contains a few example use cases"""
-
-        subprocess.check_output(['sphinx-build', '-b', 'html', './tests/example', './build/html/'])
+    subprocess.check_output(['sphinx-build', '-b', 'html', './tests/example', './build/html/'])
